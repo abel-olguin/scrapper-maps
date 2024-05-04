abbec30d048cdcd16c3916f91e797d088cb8584d
abelolguin <osmer19@hotmail.com>
2024-05-04 15:41:46 -0600

    omitting duplicates

--- src/scrapper/maps.py
+++ src/scrapper/maps.py
@@ -6,8 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-
-
 class Maps:
 
     lat = '@19.432608'
@@ -15,12 +13,16 @@ class Maps:
     api_url = "https://serpapi.com/playground?engine=google_maps&q=coffee&ll={}%2C{}%2C14z&hl=en&type=search"
     categories = ["cafeteria"]
     mapData = []
+    jsonName="data.json"
+
     def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
         self.driver = webdriver.Chrome(options=options)
+        f = open(self.jsonName)
+        self.mapData = json.load(f)
 
     def run(self):
         for category in self.categories:
@@ -28,7 +30,7 @@ class Maps:
             self.getData(url)
             time.sleep(5)
 
-        with open('data.json', 'w') as file:
+        with open(self.jsonName, 'w') as file:
             json.dump(self.mapData, file)
 
     def getData(self, url):
@@ -36,12 +38,13 @@ class Maps:
         data = r.json()
 
         for result in data["local_results"]:
-            self.mapData.append({
-                "title": result["title"] if 'title' in result else '',
-                "address": result["address"] if 'address' in result else '',
-                "phone": result["phone"] if 'phone' in result else '',
-                "website": result['website'] if 'website' in result else ''
-            })
+            if not any(item['title'] == result["title"] for item in self.mapData):
+                self.mapData.append({
+                    "title": result["title"] if 'title' in result else '',
+                    "address": result["address"] if 'address' in result else '',
+                    "phone": result["phone"] if 'phone' in result else '',
+                    "website": result['website'] if 'website' in result else ''
+                })
 
     def getApiUrl(self, category):
         self.driver.get(self.api_url.format(self.lat, self.lng))
